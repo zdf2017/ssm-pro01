f0acd7465cd6861c8fd4f95d7fa8afdfcaaf6da7
zdf2017 <1130313757@qq.com>
2017-06-12 21:55:44 +0800

    6/12

--- src/main/java/com/zdf/controller/TestController.java
+++ src/main/java/com/zdf/controller/TestController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.alibaba.fastjson.JSONObject;
 import com.zdf.controller.webBean.TableParam;
+import com.zdf.controller.webBean.TableResult;
 import com.zdf.entity.shop.Product;
 import com.zdf.services.ProductService;
 
@@ -26,11 +28,15 @@ public class TestController {
 	}
 	
 	@RequestMapping(value={"list"},method = RequestMethod.GET)
-	public JSONObject getTableData(TableParam param,Product product){
+	@ResponseBody
+	public TableResult<Product> getTableData(TableParam param,Product product){
 		System.out.println("param>>"+param);
 		List<Product> resultList = productService.getList(product);
+		TableResult<Product> tableResult = new TableResult<Product>();
+		tableResult.setRows(resultList);
+		tableResult.setTotal(resultList.size());
 		System.out.println("resultList size>>"+resultList.size());
-		return null;
+		return tableResult;
 	}
 
 }
